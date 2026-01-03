83898c4834fa209d200f1a65c4f33d4b740ed81e
Harshadbankar1596 <harshadbankar1596@gmail.com>
2026-01-03 16:39:34 +0530

    qwertrewqwertrewq

--- controller/user.controller/user.messages.controller.js
+++ controller/user.controller/user.messages.controller.js
@@ -5,10 +5,10 @@ const user_Messages_Controller = {
   async postMessages(req, res) {
     try {
       const { name, email, contact, message } = req.body;
-      if (!name || !message) {
+      if (!name || !message || !contact || !email) {
         return res
           .status(400)
-          .json({ message: "Name and message are required" });
+          .json({ message: "All Fealds are required" });
       }
       const newMessage = new MessagesModel({
         name,
